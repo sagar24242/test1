bcda13122a6e508ce6ed076c179ebfae8b66a167
sagar24242 <sagartabiyad24242@gmail.com>
2022-02-14 22:40:00 +0530

    init

---
+++ src/main/java/com/TestkillerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestkillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestkillerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/controller/SessionController.java
@@ -0,0 +1,55 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.bean.UserBean;
+
+@Controller
+public class SessionController {
+
+	@RequestMapping(value = "signup", method = RequestMethod.GET)
+	public String signup() {
+		System.out.println("i am from signup....");
+		return "Signup";// this is your view page --> html
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public String login() {
+		return "Login";// Login.jsp
+	}
+
+	@RequestMapping(value = "forgetpassword", method = RequestMethod.GET)
+	public String forgetPassword() {
+		return "ForgetPassword";
+
+	}
+
+	@RequestMapping(value="saveuser",method = RequestMethod.POST)
+	public String  saveUser(UserBean user) {
+		//fn em pwd ==> bean 
+		System.out.println(user.getFirstName());
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+		return "Login";
+	}
+	@RequestMapping(value="forgotpwd",method = RequestMethod.POST)
+	public String  forgotPwd(UserBean user) {
+		
+		System.out.println(user.getEmail());
+		
+		return "Login";
+	}
+	@RequestMapping(value="loginuser",method = RequestMethod.POST)
+	public String  loginUser(UserBean user) {
+		//fn em pwd ==> bean 
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+		return "Login";
+
+
+
+	}
+	}
+
